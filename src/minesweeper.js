95ddf03b53ff6af721b246de4272be045ec0f017
Michael Akers <akers.michael.g@gmail.com>
2017-10-20 15:00:47 -0400

    updates to minesweeper.js

--- src/minesweeper.js
+++ src/minesweeper.js
@@ -1,20 +1,30 @@
 
-const printBoard = (board) => {
-    console.log("Current_board");
-    console.log(board[0].join("|"));
-    console.log(board[1].join("|"));
-    console.log(board[2].join("|"));
+const generatePlayerBoard = (numberOfRows, numberOfColumns) => {
+    let board = []; 
+    for (let i = 0; i < numberOfRows; i++){
+        let row = [];
+        for (let j = 0; j < numberOfColumns; j++){
+            row.push(" ");
+        } board.push(row);
+    } return board
+}
 
-} 
+const generateBombBoard = (numberOfRows, numberOfColumns, numberOfBombs) => {
+    let board = [];
+    for (let i = 0; i < numberOfRows; i++){
+        let row = [];
+        for (let j = 0; j < numberOfColumns; j++){
+            row.push(null);
+        } board.push(row);
+        let numberOfBombsPlaced = 0
+        while (numberOfBombsPlaced !== numberOfBombs) {
+            let randomRowIndex = Math.random(numberOfRows) * (numberOfRows - 1) + 1;
+            let randomColumnIndex = Math.random(numberOfColumns) * (numberOfColumns - 1) + 1;
+            board[randomRowIndex][randomColumnIndex].push("B")
+            numberOfBombsPlaced++;
+        }
 
-board = [
-    [' ',' ',' '],
-    [' ',' ',' '],
-    [' ',' ',' ']
-]
-console.log(printBoard(board));
 
-board[0][1] = '1'
-board[2][2] = 'B'
 
-console.log(printBoard(board));
+    }return board
+}
