2d6de729cf404b8429c7eff22a930d272d2f3199
Michael Akers <akers.michael.g@gmail.com>
2017-10-25 17:42:01 -0400

    updated work

--- src/minesweeper.js
+++ src/minesweeper.js
@@ -26,16 +26,33 @@ let generateBombBoard = (numberOfRows, numberOfColumns, numberOfBombs) => {
     while (numberOfBombsPlaced < numberOfBombs) {
             let randomRowIndex = Math.floor(Math.random() * numberOfRows);
             let randomColumnIndex = Math.random(Math.random() * numberOfColumns);
-            board[randomRowIndex][randomColumnIndex] = 'B';
-            numberOfBombsPlaced++;
-    // this code may be redundent with placing bombs, will fix that later
-    }return board
-};
+            if ([randomRowIndex][randomColumnIndex] !== 'B'){
+                board[randomRowIndex][randomColumnIndex] = 'B';
+                numberOfBombsPlaced++;
+            }
+    }return board;
+}
+
+const getNumberOfNeighborBombs = (rowIndex, columnIndex) =>  {
+    const neighborOffsets = [[-1,-1],[-1,0],[-1,1],[0,-1],[0,1],[1,-1],[1,0],[1,1]]
+    let numberOfRows = bombBoard.length;
+    let numberOfColumns = bombBoard[0].length;
+    let numberOfBombs = 0;
+    neighborOffsets.forEach(offset => {
+        const neighborRowIndex = rowIndex + offset[0];
+        const neighborColumnIndex = columnIndex + offset[0];
+        if (neighborRowIndex >= 0 && neighborRowIndex <= numberOfRows && neighborColumnIndex >= 0 && neighborColumnIndex <= numberOfColumns) {
+            if (bombBoard[neighborRowIndex][neighborColumnIndex] === 'B') {
+                numberOfBombs++
+            }
+        }
+    }); return numberOfBombs
+}
 
 let playerBoard = generatePlayerBoard(3,4)
-let bombBoard = generateBombBoard(3,4,5)
+let bomb_Board = generateBombBoard(3,4,5)
 console.log("Player Board:");
 printBoard(playerBoard);
 console.log("Bomb Board:");
-printBoard(bombBoard);
+printBoard(bomb_Board);
 
